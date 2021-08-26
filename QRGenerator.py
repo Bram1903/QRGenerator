7da2a2b30071197e782e6e00d684dbcd2c38bdde
Bram <Bram-Dekker@live.nl>
2021-08-26 16:19:10 +0200

    Create QRGenerator.py

---
+++ QRGenerator.py
@@ -0,0 +1,14 @@
+import qrcode
+
+# Link for website
+print("Enter your URL:")
+input_data = input()
+# Creating an instance of qrcode
+qr = qrcode.QRCode(
+    version=1,
+    box_size=10,
+    border=5)
+qr.add_data(input_data)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
+img.save('qrcode001.png')
